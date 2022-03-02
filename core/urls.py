d79dc38b11b79a43cabcb3b96bc507974b542848
shubhamfam <gaikwad.shubham1311@gmail.com>
2022-03-02 11:39:33 +0530

    setting up ui

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('about/', views.about),
+    path('contactus/', views.contactus),
 ]
